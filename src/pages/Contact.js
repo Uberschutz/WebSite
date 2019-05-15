5eae7df3463f2148c2b219dc58fda84356030df6
marianne.levee <marianne.levee@epitech.eu>
2019-05-15 11:41:53 +0200

    Update changes of Connexion and Contact pages to develop on special branches [MOD] : Connexion.js / css, two spaces to connect on the website or to subscribe on it [MOD] : Contact.js / css, add informations about us and maybe something will become FAQ [ADD] : styles/Parametres.css, add color of Überschutz

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -23,18 +23,24 @@ class Contact extends Component {
                                 Beaucoup trop de personnes et d'enfants sont harcelés, arnaqués ou confrontés à du contenu inapproprié.
                                 <br/>
                                 Ainsi, nous souhaitons guider les plus jeunes afin qu'ils apprennent à utiliser Internet de la meilleure manière possible pour eux selon l'éducation que leurs parents veulent leurs donner.
-                            </h7>
+                            </h7> <br/> <br/>
+                            Notre solution :
+                            <br/><h7>
+                                Aujourd'hui, en tant que jeunes adultes, nous trouvons que certains contenus sont choquants sur Internet et que ce n'est pas assez sécurisé. <br/>
+                                Donc qu'en est-il pour un enfant ? <br/>
+                                Nous proposons un logiciel capable de monitorer une navigation Internet. <br/>
+                                De ce fait, les enfants pourront être avertis que leur recherche n'est pas adaptée pour eux selon leurs parents.
+                            </h7> <br/>
                         </h6>
+                        <br/> <br/>
                     </div>
                 </div>
                 <Form/>
+                <Faq/>
             </div>
         )
     }
 }
-
-/* Nous en tant que jeunes adultes on trouve que certains contenus sont choquants sur Internet, donc qu'en est-il d'un enfant ?
-* lieu de partage gna gna gna ... qu'on voudrait que ce soit plus sécurisé  */
 
 class Form extends Component {
     render() {
@@ -70,5 +76,63 @@ class Form extends Component {
     }
 }
 
+class Faq extends Component {
+    render() {
+        return (
+            <div className="form-align">
+                <h4>
+                    FAQ :
+                </h4> <br/>
 
-export { Contact, Form };
+                <div className="row responsive-image">
+                    <div className="col-4">
+                        <div className="list-group" id="list-tab" role="tablist">
+                            <a className="list-group-item list-group-item-action active" id="list-home-list"
+                               data-toggle="list" href="#list-home" role="tab" aria-controls="home">Menu</a>
+                            <a className="list-group-item list-group-item-action" id="list-profile-list"
+                               data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profil</a>
+                            <a className="list-group-item list-group-item-action" id="list-messages-list"
+                               data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+                            <a className="list-group-item list-group-item-action" id="list-settings-list"
+                               data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Parametres</a>
+                        </div>
+                    </div>
+                    <div className="col-8">
+                        <div className="tab-content" id="nav-tabContent">
+                            <div className="tab-pane fade show active" id="list-home" role="tabpanel"
+                                 aria-labelledby="list-home-list">
+                                <span>
+                                    Ici une question ?
+                                </span>
+                                <br/>
+                                <span className="description-txt">
+                                    LA LA REPONSE
+                                </span>
+                            </div>
+                            <div className="tab-pane fade" id="list-profile" role="tabpanel"
+                                 aria-labelledby="list-profile-list">...
+                            </div>
+                            <div className="tab-pane fade" id="list-messages" role="tabpanel"
+                                 aria-labelledby="list-messages-list">...
+                            </div>
+                            <div className="tab-pane fade" id="list-settings" role="tabpanel"
+                                 aria-labelledby="list-settings-list">...
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+/* Definir "non bloquant" et "non intrusif"
+* Comment paramétrer l'application / logiciel ?
+* Comment initialiser le controle parental sur mon appareil ?
+* Aborder question du prix (abonnement)
+* Qu'est ce que peuvent faire les parents avec logiciel (bloqué, ou avertir, tout en étant prévenus ...) par systeme d'option*/
+
+
+export { Contact, Form, Faq };
