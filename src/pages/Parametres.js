372ed384c1be5b78f63400f6e6d2d91d5297fd03
Thomas Derote <thomas.derote-parcellier@epitech.eu>
2019-05-15 20:20:13 +0200

    Change modal Add children list Change modal package Add parameters css file

--- src/pages/Parametres.js
+++ src/pages/Parametres.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
 import '../styles/bootstrap.css';
-import ModalBody from "reactstrap/es/ModalBody";
+import { Modal, Button } from 'reactstrap'
+import Card from "reactstrap/es/Card";
+import CardText from "reactstrap/es/CardText";
+import CardTitle from "reactstrap/es/CardTitle";
+import CardFooter from "reactstrap/es/CardFooter";
 
 class Parametres extends Component {
 	constructor(props) {
@@ -14,62 +18,89 @@ class Parametres extends Component {
 			name: '',
 			age: ''
 		};
-		this.openModal = this.openModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
 	}
 
 	componentWillMount() {
-		console.log('Here call to the user account API'); // or in componentDidMount
+		const newChild = [{name: 'Thomas', age: '23'}, {name: 'Marianne', age: '21'}];
+
+		this.setState({childrens: newChild})
+		console.log('Here call to the user account API to load children'); // or in componentDidMount
 	}
 
-	openModal() {
-		this.setState({showModal: true});
+	// componentDidMount() {
+	// 	const newChild = [{name: 'Thomas', age: '23'}];
+	//
+	// 	this.setState({childrens: newChild})
+	// 	console.log('Here call to the user account API to load children'); // or in componentDidMount
+	// }
+
+	toggleModal() {
+		this.setState({showModal: !this.state.showModal});
 	}
 
-	closeModal() {
-		this.setState({showModal: false});
-		console.log('closing')
+	createChildren() {
+		console.log('Here call to the user account API', this.state.name, this.state.age);
 	}
 
-	saveChildren(child) {
+	saveChildren() {
 		console.log('Here call to the user account API');
 	}
 
-	deleteChildren(child) {
+	editChildren() {
 		console.log('Here call to the user account API');
 	}
 
-	setFirstName() {
-		console.log('Here call to the user account API', this.state.name);
-		let modified = this.state.actual;
-		modified.name = this.state.name;
-		this.setState({actual: modified});
+	deleteChildren() {
+		console.log('Here call to the user account API');
+	}
+
+	setFirstName(name) {
+		console.log('Here set children name', name);
+		this.setState({name: name})
 	}
 
 	setAge(age) {
-		console.log('Here call to the user account API');
+		console.log('Here set children age', age);
+		this.setState({age: age});
 	}
 
 	render() {
         return (
-            <div>
-                <button onClick={this.openModal}>Ajouter un enfant</button>
-	            <Modal
-		            isOpen={this.state.showModal}
-		            ariaHideApp={false}
-		            // onAfterOpen={this.afterOpenModal}
-		            onRequestClose={this.closeModal}
-		            // style={customStyles}
-		            contentLabel="Example Modal">
-		                <button onClick={this.closeModal}>Fermer</button>
-			            <form onSubmit={() => this.setFirstName()}>
-				            <label>
-					            Name:
-					            <input type="text" value={this.state.name} onChange={(name) => this.setState({name: name.target.value})}/>
-				            </label>
-				            <input type="submit" value="Submit" />
-			            </form>
+            <div className="card">
+                {/*<button onClick={this.toggleModal}>Ajouter un enfant</button>*/}
+	            <Modal isOpen={this.state.showModal} toggle={this.toggleModal}>
+					<button onClick={this.toggleModal}>Fermer</button>
+					<form onSubmit={() => this.createChildren()}>
+						<label>
+							Name<br/>
+							<input type="text" value={this.state.name} onChange={(name) => this.setFirstName(name.target.value)}/>
+						</label>
+						<br/>
+						<label>
+							Age<br/>
+							<input type="text" value={this.state.age} onChange={(age) => this.setAge(age.target.value)}/>
+						</label>
+						<br/><br/>
+						<input type="submit" value="Submit" />
+					</form>
 	            </Modal>
+				{
+					this.state.childrens.map(child => {
+						console.log(child);
+						return (
+							<Card body inverse style={{ backgroundColor: '#333', borderColor: '#333', margin: 10, width: '50%', alignItems: 'center', alignSelf: 'center' }}>
+								<CardTitle>{child.name}</CardTitle>
+								<CardText>{child.age}</CardText>
+								<CardFooter>
+									<Button onClick={(child) => this.deleteChildren(child)}>Delete</Button>
+									<Button onClick={(child) => this.editChildren(child)}>Edit</Button>
+								</CardFooter>
+							</Card>
+						);
+					})
+				}
+				<Button color="primary" style={{ width: '50%', alignSelf: 'center'}} onClick={this.toggleModal}>New</Button>
             </div>
         )
     }
