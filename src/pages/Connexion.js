058aa55e8130a796a2fd09c3b56ef3a34a03b120
marianne.levee <marianne.levee@epitech.eu>
2019-05-17 10:42:24 +0200

    Add redirection to the connection page [MOD] : Connexion.js / css, redirection of the button Continue to homepage or profil Need to display an error message with regex on field [MOD] : Footer.js, add redirection to Contact page on the button

--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import '../styles/bootstrap.css';
 import '../styles/Connexion.css';
 
+import { Link } from 'react-router-dom';
+
 class Connexion extends Component {
+
+    /* constructor(){
+        super();
+        this.state = {value: ''};
+        this.onChange = this.onChange.bind(this)
+    }
+
+    onChange(e){
+        const re = /^[0-9\b]+$/;
+        if (e.target.value === '' || re.test(e.target.value)) {
+            this.setState({value: e.target.value})
+            console.log(re.test(e.target.value));
+        }
+    } */
+
     render() {
         return (
             <div>
                 <div className="row responsive-image">
                     <div className="col">
+                        <br/>
                         <div className="card text-white bg-dark box-center">
                             <div className="card-body bg-dark item-align">
                                 <h5 className="card-title text-align">
@@ -28,18 +46,53 @@ class Connexion extends Component {
                                     </div>
                                     <input type="password" className="form-control" aria-label="Email"/>
                                 </div>
-                                <button type="button" className="btn btn-light">Continue</button>
+                                <Link to ='/'>
+                                    <button type="button" className="btn btn-primary align-continue">Continue</button>
+                                </Link>
                             </div>
                         </div>
+                        <br/>
                     </div>
                     <div className="col">
+                        <br/>
                         <div className="card text-white bg-dark box-center">
                             <div className="card-body bg-dark item-align">
                                 <h5 className="card-title text-align">
                                     Inscription
                                 </h5>
+                                <p className="card-text">
+                                    Merci de remplir ces champs pour continuer
+                                </p>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="inputGroup-sizing-default">Nom</span>
+                                    </div>
+                                    <input /*value={this.state.value} onChange={this.onChange}*/ type="text" className="form-control" aria-label="Nom"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="inputGroup-sizing-default">Prénom</span>
+                                    </div>
+                                    <input /*value={this.state.value} onChange={this.onChange}*/ type="text" className="form-control" aria-label="Prénom"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                                    </div>
+                                    <input type="text" className="form-control" aria-label="Email"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="inputGroup-sizing-default">Password</span>
+                                    </div>
+                                    <input type="password" className="form-control" aria-label="Email"/>
+                                </div>
+                                <Link to ='/Profil'>
+                                    <button type="button" className="btn btn-primary align-continue">Continue</button>
+                                </Link>
                             </div>
                         </div>
+                        <br/>
                     </div>
                 </div>
             </div>
