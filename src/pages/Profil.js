b33334ee74f6d00bf47d77f35803544c2cea0052
marianne.levee <marianne.levee@epitech.eu>
2019-05-22 12:56:50 +0200

    Begin the profile page [MOD / ADD] : Profil.js / css, need to change Prenom to the first name of the parent + number of childrens + buy pack etc + table to complete

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 import '../styles/bootstrap.css';
 
+import { Icon } from 'antd';
+
 class Profil extends Component {
     render() {
         return (
-            <div>
-                <span>Hello profil</span>
+            <div className="description-txt">
+                <h4 className="name-font">
+                    Prénom
+                </h4> <br/>
+                <h6>
+                    Enfants protégés par Überschutz : X <br/> <br/>
+                    Abonnement utilisé : Premium <br/> <br/>
+                    Echéancier de paiement : <br/>
+                </h6> <br/>
+                <table className="table">
+                    <thead className="table-primary">
+                    <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">Nom du prélèvement</th>
+                        <th scope="col">Payé ?</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">22/05/2019</th>
+                        <td>Achat d'Überschutz Premium</td>
+                        <td>
+                            <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">25/05/2019</th>
+                        <td>Prélèvement n°1 Überschutz Premium</td>
+                        <td>En cours ...</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">25/06/2019</th>
+                        <td>Prélèvement n°2 Überschutz Premium</td>
+                        <td>Prochainement</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <br/>
             </div>
         )
     }
