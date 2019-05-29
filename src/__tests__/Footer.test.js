9ccfea41ab4abd89c96cead4f76544bcfd764c21
Thomas Derote <thomas.derote-parcellier@epitech.eu>
2019-05-29 17:15:33 +0200

    Fix/Update Footer component unit test Update Footer component snapshot

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from '../pages/Footer';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Footer />, div);
+    ReactDOM.render(<Router><Footer lang='fr'/></Router>, div);
     expect(div).toMatchSnapshot();
     ReactDOM.unmountComponentAtNode(div);
 });
