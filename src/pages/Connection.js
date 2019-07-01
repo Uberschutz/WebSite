d9bbad68a3aa5c049921273854531f9bd62620aa
marianne.levee <marianne.levee@epitech.eu>
2019-07-01 22:43:56 +0200

    Separate Connection and Registration components

--- src/pages/Connection.js
+++ src/pages/Connection.js
@@ -59,11 +59,9 @@ class Connection extends Component {
                         <div className="card text-white bg-dark box-center">
                             <div className="card-body bg-dark item-align text-align">
                                 <h5 className="card-title text-align">
-                                    {/*Authentication*/}
                                     {displayContent(this.props.lang, i++, 'connexion')}
                                 </h5>
                                 <p className="card-text">
-                                    {/*Please enter an email and a password to continue*/}
                                     {displayContent(this.props.lang, i++, 'connexion')}
                                 </p>
                                 <div className="input-group mb-3">
@@ -85,55 +83,106 @@ class Connection extends Component {
                         </div>
                         <br/>
                     </div>
-                    <div className="col">
-                        <br/>
-                        <div className="card text-white bg-dark box-center">
-                            <div className="card-body bg-dark item-align text-align">
-                                <h5 className="card-title text-align">
-                                    {/*Inscription*/}
-                                    {displayContent(this.props.lang, i++, 'connexion')}
-                                </h5>
-                                <p className="card-text">
-                                    {/*Merci de remplir ces champs pour continuer*/}
-                                    {displayContent(this.props.lang, i++, 'connexion')}
-                                </p>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'connexion')}</span>
-                                    </div>
-	                                {this.state.alphaName ? <input value={this.state.name} onChange={(name) => this.onChangeName(name.target.value)} type="text" className="form-control form-box-error" aria-label="Nom"/> :
-                                    <input value={this.state.name} onChange={(name) => this.onChangeName(name.target.value)} type="text" className="form-control" aria-label="Nom"/>}
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'connexion')}</span>
-                                    </div>
-	                                {this.state.alphaSurname ? <input value={this.state.value} onChange={(surname) => this.onChangeSurname(surname.target.value)} type="text" className="form-control form-box-error" aria-label="Prénom"/> :
-                                    <input value={this.state.value} onChange={(surname) => this.onChangeSurname(surname.target.value)} type="text" className="form-control" aria-label="Prénom"/>}
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
-                                    </div>
-                                    <input value={this.state.email} onChange={(email) => this.onChangeEmail(email.target.value)} type="text" className="form-control" aria-label="Email"/>
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'connexion')}</span>
-                                    </div>
-                                    <input value={this.state.password} onChange={(password) => this.onChangePass(password.target.value)} type="password" className="form-control" aria-label="Email"/>
-                                </div>
-                                <Link to ='/Profil'>
-                                    <button type="button" className="btn btn-primary">{displayContent(this.props.lang, i++, 'connexion')}</button>
-                                </Link>
-	                            <br/>
-	                            {this.state.alphaSurname || this.state.alphaName ?
-		                            <span className="address text-danger">{displayContent(this.props.lang, i, 'connexion')}</span> : null}
+                    <Registration lang={this.props.lang}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Registration extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            surname: '',
+            email: '',
+            password: '',
+            alphaName: false,
+            alphaSurname: false
+        };
+    }
+
+    static isAlpha(char) {
+        const re = /^[a-zA-Z]+$/g;
+        return re.test(char);
+    }
+
+    onChangeName(name) {
+        if (name && !Connection.isAlpha(name)) {
+            this.setState({alphaName: true});
+        } else {
+            this.setState({alphaName: false});
+        }
+        this.setState({name: name});
+    }
+
+    onChangeSurname(surname) {
+        if (surname && !Connection.isAlpha(surname)) {
+            this.setState({alphaSurname: true});
+        } else {
+            this.setState({alphaSurname: false});
+        }
+        this.setState({surname: surname});
+    }
+
+    onChangeEmail(email) {
+        this.setState({email: email});
+    }
+
+    onChangePass(pass) {
+        this.setState({password: pass});
+    }
+
+    render() {
+        let i = 0;
+        return (
+            <div className="col">
+                <br/>
+                <div className="card text-white bg-dark box-center">
+                    <div className="card-body bg-dark item-align text-align">
+                        <h5 className="card-title text-align">
+                            {displayContent(this.props.lang, i++, 'registration')}
+                        </h5>
+                        <p className="card-text">
+                            {displayContent(this.props.lang, i++, 'registration')}
+                        </p>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'registration')}</span>
                             </div>
+                            {this.state.alphaName ? <input value={this.state.name} onChange={(name) => this.onChangeName(name.target.value)} type="text" className="form-control form-box-error" aria-label="Nom"/> :
+                                <input value={this.state.name} onChange={(name) => this.onChangeName(name.target.value)} type="text" className="form-control" aria-label="Nom"/>}
                         </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'registration')}</span>
+                            </div>
+                            {this.state.alphaSurname ? <input value={this.state.value} onChange={(surname) => this.onChangeSurname(surname.target.value)} type="text" className="form-control form-box-error" aria-label="Prénom"/> :
+                                <input value={this.state.value} onChange={(surname) => this.onChangeSurname(surname.target.value)} type="text" className="form-control" aria-label="Prénom"/>}
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                            </div>
+                            <input value={this.state.email} onChange={(email) => this.onChangeEmail(email.target.value)} type="text" className="form-control" aria-label="Email"/>
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">{displayContent(this.props.lang, i++, 'registration')}</span>
+                            </div>
+                            <input value={this.state.password} onChange={(password) => this.onChangePass(password.target.value)} type="password" className="form-control" aria-label="Email"/>
+                        </div>
+                        <Link to ='/Profile'>
+                            <button type="button" className="btn btn-primary">{displayContent(this.props.lang, i++, 'registration')}</button>
+                        </Link>
                         <br/>
+                        {this.state.alphaSurname || this.state.alphaName ?
+                            <span className="address text-danger">{displayContent(this.props.lang, i, 'registration')}</span> : null}
                     </div>
                 </div>
+                <br/>
             </div>
         )
     }
