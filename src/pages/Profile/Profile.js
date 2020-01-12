8811925d6c611a18a7c4ea9f20d79386225ddd26
Thomas <thomas.derote-parcellier@epitech.eu>
2020-01-12 02:04:37 +0100

    Add account data download for GDPR requirements (in json format)
    Replace arrow function in render with binded ones (for optimization)

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -9,6 +9,8 @@ import Unauthorized from "../Unauthorized";
 
 import axios from 'axios';
 
+import fileDownload from "js-file-download";
+
 class Profile extends Component {
 	constructor(props) {
 		super(props);
@@ -23,7 +25,14 @@ class Profile extends Component {
 			subscribed: false,
 			subscription: '',
 			token: null
-		}
+		};
+		// this._handleKeyPressed = this._handleKeyPressed.bind(this);
+		this.onChangeFirstname = this.onChangeFirstname.bind(this);
+		this.onChangeLastname = this.onChangeLastname.bind(this);
+		this.changeFirstName = this.changeFirstName.bind(this);
+		this.changeLastName = this.changeLastName.bind(this);
+		this.getAccountData = this.getAccountData.bind(this);
+		this.deleteAccount = this.deleteAccount.bind(this);
 	}
 
 	componentDidMount() {
@@ -97,13 +106,13 @@ class Profile extends Component {
 
 	onChangeFirstname(value) {
 		this.setState({
-			firstname: value
+			firstname: value.target.value
 		})
 	}
 
 	onChangeLastname(value) {
 		this.setState({
-			lastname: value
+			lastname: value.target.value
 		})
 	}
 
@@ -153,9 +162,13 @@ class Profile extends Component {
 				'x-access-token': this.state.token
 			}
 		}).then(response => {
-
+			try {
+				fileDownload(JSON.stringify(response.data), 'data.json');
+			} catch (e) {
+				console.log(e);
+			}
 		}).catch(err => {
-
+			console.log(err);
 		})
 	}
 
@@ -179,8 +192,8 @@ class Profile extends Component {
 							{displayContent(this.state.lang, i++, 'profile')}
 						</h4>
 						<div className="input-group input-group-sm col-sm-2">
-							<input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onKeyPress={(e) => { this._handleKeyPressed(e, "lastname")}} value={this.state.lastname} onChange={(value) => this.onChangeLastname(value.target.value)}/>
-							<button className="btn btn-primary btn-sm" onClick={() => this.changeLastName()}>OK</button>
+							<input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onKeyPress={(e) => { this._handleKeyPressed(e, "lastname")}} value={this.state.lastname} onChange={this.onChangeLastname}/>
+							<button className="btn btn-primary btn-sm" onClick={this.changeLastName}>OK</button>
 						</div> <br/>
 					</div>
 					<div className="row button-footerP">
@@ -188,8 +201,8 @@ class Profile extends Component {
 							{displayContent(this.state.lang, i++, 'profile')}
 						</h5>
 						<div className="input-group input-group-sm col-sm-2">
-							<input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onKeyPress={(e) => { this._handleKeyPressed(e, "firstname")}} value={this.state.firstname} onChange={(value) => this.onChangeFirstname(value.target.value)}/>
-							<button className="btn btn-primary btn-sm" onClick={() => this.changeFirstName()}>OK</button>
+							<input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onKeyPress={(e) => { this._handleKeyPressed(e, "firstname")}} value={this.state.firstname} onChange={this.onChangeFirstname}/>
+							<button className="btn btn-primary btn-sm" onClick={this.changeFirstName}>OK</button>
 						</div><br/>
 					</div>
 				    <h6>
@@ -228,8 +241,8 @@ class Profile extends Component {
 				    </table>
 				    <br/>
 				    <div className="row txt-align">
-						<button className="col-2 btn btn-primary button-footerP" onClick={() => this.getAccountData()}>{displayContent(this.state.lang, i++, 'profile')}</button>
-						<button className="col-2 btn btn-danger button-footerP" onClick={() => this.deleteAccount()}>{displayContent(this.state.lang, i++, 'profile')}</button>
+						<button className="col-2 btn btn-primary button-footerP" onClick={this.getAccountData}>{displayContent(this.state.lang, i++, 'profile')}</button>
+						<button className="col-2 btn btn-danger button-footerP" onClick={this.deleteAccount}>{displayContent(this.state.lang, i++, 'profile')}</button>
 					</div>
 			    </div>
 		    )
