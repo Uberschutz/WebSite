b91140d2a390f6e46187c6087eb4e9c88d078531
marianne.levee <marianne.levee@epitech.eu>
2020-02-16 03:58:12 +0100

    Add new page and component for in coming pages
    Add pictures in progress
    Translate message
    New component and functions to display in coming message

---
+++ src/pages/InComing.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import '../styles/Connection.css'
+import {displayContent} from "../utils/translationDisplay";
+import inComing from "../assets/fogg-in-coming.png";
+
+export default class InComing extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lang: 'fr',
+        };
+    }
+
+    componentDidMount() {
+        if (this.props.base) {
+            const { base: { language} } = this.props;
+            this.setState({
+                lang: language
+            });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.base && (this.props.base.language !== this.state.lang)) {
+            // console.log(prevProps, this.props);
+            this.setState({lang: this.props.base.language}, () => console.log('re'));
+        }
+    }
+
+    render() {
+        let i = 0;
+        return (
+            <div>
+                <h1 className="text-xl-center forbidden">
+                    {displayContent(this.state.lang, i++, 'inComing')}
+                </h1> <br/>
+                <img src={inComing} alt="inComing"/>
+            </div>
+        );
+    }
+}
