a32e2a25e0838c688f9ef1903765e1080bcf43de
marianne.levee <marianne.levee@epitech.eu>
2020-02-18 20:51:48 +0100

    Translate subscription page in english

--- src/pages/Subscription/Subscription.js
+++ src/pages/Subscription/Subscription.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import axios from 'axios';
+import { displayContent } from '../../utils/translationDisplay';
+
 import '../../styles/Subscription.css';
 import addOn from '../../assets/icons8-add-on-80.png';
 import software from '../../assets/icons8-logiciel-96.png';
@@ -13,6 +15,7 @@ export default class Subscription extends Component {
         super(props);
 
         this.state = {
+            lang: 'fr',
             account_licence: null,
             available_licences: []
         };
@@ -23,6 +26,10 @@ export default class Subscription extends Component {
 
     componentDidMount() {
         if (this.props.base) {
+            const { base: { language} } = this.props;
+            this.setState({
+                lang: language
+            });
             axios.get('/get_available_licences', {
                 headers: {
                     'x-access-token': this.props.base.token
@@ -45,6 +52,13 @@ export default class Subscription extends Component {
             }).catch(err => {
                 console.log(err);
             });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.base && (this.props.base.language !== this.state.lang)) {
+            // console.log(prevProps, this.props);
+            this.setState({lang: this.props.base.language}, () => console.log('re'));
         }
     }
 
@@ -92,70 +106,66 @@ export default class Subscription extends Component {
     }
 
     render() {
+        let i = 0;
         return (
             <div className="fixed-size">
                 <div className="title-intro">
-                    <h4>Logiciels et plug-in</h4> <br/>
+                    <h4>{displayContent(this.state.lang, i++, 'subscription')}</h4> <br/>
                 </div>
                 <div>
                     <div className="row">
                         <div className="col-sm">
                             <img className="image-border" src={addOn} alt="addOn"/> <br/>
                             <span className="title-products">Add-on</span> <br/>
-                            One of two columns <br/>
-                            Permet d'analyser votre navigation internet <br/>
-                            Avoir un compte-rendu de toxicité <br/>
-                            De gérer son compte <br/>
-                            GRATUIT <br/> <br/>
-                            <button type="button" className="btn btn-success">Télécharger</button> <br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            <button type="button" className="btn btn-success button-border">{displayContent(this.state.lang, i++, 'subscription')}</button> <br/>
                         </div>
                         <div className="col-sm">
                             <img className="image-border" src={software} alt="software"/> <br/>
-                            <span className="title-products">Logiciel</span> <br/>
-                            One of two columns <br/>
-                            Permet de gérer son comptes sur un bureau de pc <br/>
-                            Avoir un compte-rendu de toxicité <br/>
-                            Possibilité d'achat <br/> <br/>
-                            <button type="button" className="btn btn-warning">Télécharger</button> <br/>
+                            <span className="title-products">{displayContent(this.state.lang, i++, 'subscription')}</span> <br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            <button type="button" className="btn btn-warning button-border">{displayContent(this.state.lang, i++, 'subscription')}</button> <br/>
                         </div>
                     </div>
                 </div> <br/>
                 <div className="title-intro">
-                    <h4>Licences</h4> <br/>
+                    <h4>{displayContent(this.state.lang, i++, 'subscription')}</h4> <br/>
                 </div>
                 <div>
                     <div className="row">
                         <div className="col-xl-4">
                             <img className="image-border" src={family} alt="family"/> <br/>
-                            <span className="title-products">Licence Basique</span> <br/>
-                            One of three columns <br/>
-                            Basique : <br/>
-                            - 1 appareils protégé et analysé <br/>
-                            - convient à des familles qui ont 1 enfant à protéger <br/>
-                            - à partir de 20€ <br/> <br/>
-                            <button type="button" className="btn btn-info" onClick={this.addLicence} data-idx={0}>Acheter</button> <br/>
+                            <span className="title-products">{displayContent(this.state.lang, i++, 'subscription')}</span> <br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            <button type="button" className="btn btn-info button-border" onClick={this.addLicence} data-idx={0}>{displayContent(this.state.lang, i++, 'subscription')}</button> <br/>
                         </div>
                         <div className="col-xl-4">
                             <img className="image-border" src={familyPlus} alt="familyPlus"/> <br/>
-                            <span className="title-products">Licence Famille Plus</span> <br/>
-                            One of three columns <br/>
-                            Famille : <br/>
-                            - 3 appareils protégés et analysés <br/>
-                            - convient à des familles qui ont 1 ou 2 enfants à protéger <br/>
-                            - à partir de 35€ <br/> <br/>
-                            <button type="button" className="btn btn-info" onClick={this.addLicence} data-idx={1}>Acheter</button> <br/>
+                            <span className="title-products">{displayContent(this.state.lang, i++, 'subscription')}</span> <br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            <button type="button" className="btn btn-info button-border" onClick={this.addLicence} data-idx={1}>{displayContent(this.state.lang, i++, 'subscription')}</button> <br/>
                         </div>
                         <div className="col-xl-4">
                             <img className="image-border" src={newLicence} alt="newLicence"/> <br/>
-                            <span className="title-products">Licence Premium</span> <br/>
-                            One of three columns <br/>
-                            Premium : <br/>
-                            - 5 appareils protégés et analysés <br/>
-                            - convient à des familles qui ont plus de 2 enfants à protéger <br/>
-                            - à partir de 50€ <br/>
-                            Avantages ! <br/>
-                            - Profitez de ... <br/> <br/>
-                            <button type="button" className="btn btn-info" onClick={this.addLicence} data-idx={2}>Acheter</button> <br/> <br/> <br/> <br/>
+                            <span className="title-products">{displayContent(this.state.lang, i++, 'subscription')}</span> <br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            {displayContent(this.state.lang, i++, 'subscription')}<br/>
+                            <button type="button" className="btn btn-info image-border button-border" onClick={this.addLicence} data-idx={2}>{displayContent(this.state.lang, i++, 'subscription')}</button> <br/>
                         </div>
                     </div>
                 </div>
