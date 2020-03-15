55a1f0fdb836c425f14d83c35d6bc49f488f742b
Thomas Derote <thomas.derote-parcellier@epitech.eu>
2020-03-15 15:07:41 +0000

    Add service data according to the new option available for children data
    Add generic function that handle all request responses and forward it

--- Server.js
+++ Server.js
@@ -15,126 +15,73 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'dev') {
 }
 
 app.post('/get_data', (req, res) => {
+	let data = `token=${credentials.token}&type=text`;
 	if (req.body.discordId) {
-		axios.post(`${credentials.api_ip}/collect`, `token=${credentials.token}&type=text&userId=${req.body.discordId}`, {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}
-		}).then(response => {
-			res.send(response.data.flagsPercentage);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).send('ko');
-		})
-	} else {
-		axios.post(`${credentials.api_ip}/collect`, `token=${credentials.token}&type=text`, {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}
-		}).then(response => {
-			res.send(response.data.flagsPercentage);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).send('ko');
-		})
+		data += `&userId=${req.body.discordId}`
 	}
-});
-
-app.post('/subscribe_newsletter', (req, res) => {
-	// console.log(req.body, req.data);
-	axios.post(`http://${server_url}:8081/subscribe_newsletter`, {
-		email: req.body.email,
-		name: req.body.name
+	if (req.body.service) {
+		data += `&service=${req.body.service}`
+	}
+	axios.post(`${credentials.api_ip}/collect`, data, {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
 	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
+		res.send(response.data.flagsPercentage);
 	}).catch(err => {
-		console.log(err);
-		if (err.response && err.response.data)
-			res.status(500).send(err.response.data);
-		else if (err.response && err.response.statusText)
-			res.status(500).send(err.response.statusText);
-		else
-			res.status(500).send('Unknown Internal error');
+		res.status(err.response.status).send(err.response.data);
 	});
 });
 
-app.post('/unsubscribe_newsletter', (req, res) => {
+app.post('/subscribe_newsletter', async (req, res) => {
 	// console.log(req.body, req.data);
-	axios.post(`http://${server_url}:8081/unsubscribe_newsletter`, {
+	let result = await axios.post(`http://${server_url}:8081/subscribe_newsletter`, {
+		email: req.body.email,
+		name: req.body.name
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
+});
+
+app.post('/unsubscribe_newsletter', async (req, res) => {
+	let result = axios.post(`http://${server_url}:8081/unsubscribe_newsletter`, {
 		email: req.body.email,
 		name: req.body.name
 	}, {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		if (err.response && err.response.data)
-			res.status(500).send(err.response.data);
-		else if (err.response && err.response.statusText)
-			res.status(500).send(err.response.statusText);
-		else
-			res.status(500).send('Unknown Internal error');
-	});
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
 
-app.post('/register', (req, res) => {
-	axios.post(`http://${server_url}:8081/register`, {
+app.post('/register', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/register`, {
 		email: req.body.email,
 		passwd: req.body.passwd,
 		lastname: req.body.lastname,
 		firstname: req.body.firstname
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		if (err.response && err.response.data)
-			res.status(500).send(err.response.data);
-		else if (err.response && err.response.statusText)
-			res.status(500).send(err.response.statusText);
-		else
-			res.status(500).send('Unknown Internal error');
-	});
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result)
 });
 
-app.post('/verifyaccount', (req, res) => {
-	axios.post(`http://${server_url}:8081/verifyaccount`, {
+app.post('/verifyaccount', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/verifyaccount`, {
 		id: req.body.id
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		if (err.response && err.response.data)
-			res.status(500).send(err.response.data);
-		else if (err.response && err.response.statusText)
-			res.status(500).send(err.response.statusText);
-		else
-			res.status(500).send('Unknown Internal error');
-	});
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/connect', (req, res) => {
-	axios.post(`http://${server_url}:8081/connect`, {
+app.post('/connect', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/connect`, {
 		email: req.body.email,
 		passwd: req.body.passwd
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(err);
-	});
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/children', (req, res) => {
-	axios.post(`http://${server_url}:8081/children`, {
+app.post('/children', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/children`, {
 		action: req.body.action,
 		name: req.body.name,
 		newName: (req.body.newName ? req.body.newName : ''),
@@ -145,116 +92,76 @@ app.post('/children', (req, res) => {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.statusText}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/rename', (req, res) => {
-	axios.post(`http://${server_url}:8081/rename`, {
+app.post('/rename', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/rename`, {
 		key: req.body.key,
 		value: req.body.value
 	}, {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/delete_account', (req, res) => {
-	axios.post(`http://${server_url}:8081/delete_account`, '', {
+app.post('/delete_account', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/delete_account`, '', {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.get('/gdpr', (req, res) => {
-	axios.get(`http://${server_url}:8081/gdpr`, {
+app.get('/gdpr', async (req, res) => {
+	let result = await axios.get(`http://${server_url}:8081/gdpr`, {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.get('/get_available_licences', (req, res) => {
-	axios.get(`http://${server_url}:8081/get_available_licences`,{
+app.get('/get_available_licences', async (req, res) => {
+	let result = await axios.get(`http://${server_url}:8081/get_available_licences`,{
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.get('/get_subscription', (req, res) => {
-	axios.get(`http://${server_url}:8081/get_subscription`,{
+app.get('/get_subscription', async (req, res) => {
+	let result = await axios.get(`http://${server_url}:8081/get_subscription`,{
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		// console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		// console.log(err);
-		res.status(err.response.status).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/subscribe', (req, res) => {
-	axios.post(`http://${server_url}:8081/subscribe`, {
+app.post('/subscribe', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/subscribe`, {
 		subscription: req.body.subscription
 	}, {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		// console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		// console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
-app.post('/unsubscribe', (req, res) => {
-	axios.post(`http://${server_url}:8081/unsubscribe`, '', {
+app.post('/unsubscribe', async (req, res) => {
+	let result = await axios.post(`http://${server_url}:8081/unsubscribe`, '', {
 		headers: {
 			'x-access-token': req.headers['x-access-token']
 		}
-	}).then(response => {
-		// console.log(response.data);
-		res.send(response.data);
-	}).catch(err => {
-		// console.log(err);
-		res.status(500).send(`An error occurred: ${err.response.data}`);
-	})
+	}).then(response => response).catch(err => err.response);
+	forward_response(res, result);
 });
 
 // app.post('/set_subscription', (req, res) => {
@@ -276,6 +183,17 @@ app.post('/unsubscribe', (req, res) => {
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve( __dirname, "./build/index.html" ));
 });
+
+function forward_response(res, promise) {
+	/*console.log({
+		method: promise.request.method,
+		url: promise.request.res.responseUrl,
+		status: promise.status,
+		statusText: promise.statusText,
+		data: promise.data
+	});*/
+	res.status(promise.status).send(promise.data);
+}
 
 app.listen(8080, () =>
 	console.log('Server listening on 8080')
