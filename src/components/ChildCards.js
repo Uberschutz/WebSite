3f6469cdbcbc66f954e54e569ea3a2a5343334b7
Thomas Derote <thomas.derote-parcellier@epitech.eu>
2020-06-19 16:39:27 +0200

    Update utils components for optimization
    Add functions to utils components to replace arrow function calls

--- src/components/ChildCards.js
+++ src/components/ChildCards.js
@@ -7,16 +7,33 @@ import CardTitle from "reactstrap/lib/CardTitle";
 import Icon from "antd/lib/icon/index";
 
 export class DisplayChildrenCard extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.editChildren = this.editChildren.bind(this);
+		this.deleteChildren = this.deleteChildren.bind(this);
+	}
+
+
+	editChildren() {
+		this.props.editChildren(this.props.child);
+	}
+
+	deleteChildren() {
+		this.props.deleteChildren(this.props.child)
+	}
+
 	render() {
 		return (
 			<Card body inverse className="child-card">
 				<CardTitle>{this.props.title} : {this.props.child.name}</CardTitle>
 				<CardText>Age : {this.props.child.age}</CardText>
 				<CardFooter>
-					<Button className="btn-mod btn btn-danger" onClick={() => this.props.deleteChildren(this.props.child)}>
+					<Button className="btn-mod btn btn-danger" onClick={this.deleteChildren}>
 						{this.props.delete} <Icon type="delete" className="size-icon"/>
 					</Button>
-					<Button className="btn-mod btn btn-success" onClick={() => this.props.editChildren(this.props.child)}>
+					<Button className="btn-mod btn btn-success" onClick={this.editChildren}>
 						{this.props.edit} <Icon type="edit" className="size-icon" />
 					</Button>
 				</CardFooter>
