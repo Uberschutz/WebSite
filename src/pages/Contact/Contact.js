eda749bfc10eaf256f533232ea99916ab707eb99
Thomas Derote <thomas.derote-parcellier@epitech.eu>
2020-06-23 18:35:35 +0200

    Fix the environment variable to enable Google Analytics

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -28,7 +28,7 @@ class Contact extends Component {
     }
 
 	componentDidMount() {
-		if (process.env.ANALYTICS === 'true') {
+		if (process.env.REACT_APP_ANALYTICS === 'true') {
 			ReactGA.pageview(window.location.pathname + window.location.search);
 		}
     	if (this.props.base) {
